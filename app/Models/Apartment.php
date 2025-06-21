34fc0883e2a830df9af064743ac5a5dacebc1437
dezareo <dezareo@gmail.com>
2025-06-21 12:23:09 +0200

    Fill Model Appartment

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Apartment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'location',
+        'price_per_night',
+        'number_of_beds',
+        'image',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
