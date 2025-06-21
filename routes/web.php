428fc80a30f0863cb9d9824a7441523b379ac7b8
dezareo <dezareo@gmail.com>
2025-06-21 12:28:18 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ApartmentController; // Dodaj ovu liniju
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Rute za upravljanje apartmanima
+    Route::resource('apartments', ApartmentController::class);
 });
 
 require __DIR__.'/auth.php';
